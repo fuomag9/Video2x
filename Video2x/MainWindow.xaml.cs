8fde806c7760c54b9949117c7cc93942942f5e52
fuomag9 <fuomag9@hotmail.it>
2019-01-19 17:56:23 +0100

    commit before algorithm implementation

--- Video2x/MainWindow.xaml.cs
+++ Video2x/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Video2x
 {
@@ -23,6 +24,22 @@ namespace Video2x
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            CommonFileDialogResult result = dialog.ShowDialog();
+            textbox_folder.Text = dialog.FileName;
+        }
+
+        private void Save_button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            saveFileDialog.ShowDialog();
+            textbox_save.Text=saveFileDialog.FileName;
+
         }
     }
 }
